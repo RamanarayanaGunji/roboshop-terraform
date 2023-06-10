8420efe4eeb1e37b8f85e2843f50acbff2e3dd99
Ramanarayana <gunjirama1999@gmail.com>
2023-06-10 15:39:05 +0530

    Creation of all Terraform_instance_creation

--- servers.tf
+++ servers.tf
@@ -1,6 +1,6 @@
 
 data "aws_ami" "centos" {
-  executable_users = [" 973714476881"]
+  executable_users = ["973714476881"]
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
   }

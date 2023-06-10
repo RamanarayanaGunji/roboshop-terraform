3c7cd38cf4c01d6f1a8527873d032161464bbba7
Ramanarayana <gunjirama1999@gmail.com>
2023-06-10 15:59:48 +0530

    creation of domain automaticallyBy  terraform

--- servers.tf
+++ servers.tf
@@ -22,6 +22,16 @@ resource "aws_instance" "frontend" {
 //output "frontend" {
  //value = aws_instance.frontend.public_ip
 //}
+
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z08051092LKB6WUQCW0K4"
+  // zone id taken from domain tab itself (edit hosted zone itself)
+  name    = "frontend.devopsb72r.online"
+  type    = "A"
+  ttl     = 30  //300 to 30
+  records = [aws_instance.frontend.private_ip]
+}
+
 resource "aws_instance" "mongodb" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
@@ -29,6 +39,15 @@ resource "aws_instance" "mongodb" {
   tags = {
     Name = "mongodb"
   }
+}
+
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z08051092LKB6WUQCW0K4"
+  // zone id taken from domain tab itself (edit hosted zone itself)
+  name    = "mongodb.devopsb72r.online"
+  type    = "A"
+  ttl     = 30  //300 to 30
+  records = [aws_instance.mongodb.private_ip]
 }
 
 resource "aws_instance" "catalogue" {
@@ -40,6 +59,15 @@ resource "aws_instance" "catalogue" {
   }
 }
 
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z08051092LKB6WUQCW0K4"
+  // zone id taken from domain tab itself (edit hosted zone itself)
+  name    = "catalogue.devopsb72r.online"
+  type    = "A"
+  ttl     = 30  //300 to 30
+  records = [aws_instance.catalogue.private_ip]
+}
+
 resource "aws_instance" "redis" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
@@ -47,6 +75,15 @@ resource "aws_instance" "redis" {
   tags = {
     Name = "redis"
   }
+}
+
+resource "aws_route53_record" "redis" {
+  zone_id = "Z08051092LKB6WUQCW0K4"
+  // zone id taken from domain tab itself (edit hosted zone itself)
+  name    = "redis.devopsb72r.online"
+  type    = "A"
+  ttl     = 30  //300 to 30
+  records = [aws_instance.redis.private_ip]
 }
 
 resource "aws_instance" "user" {
@@ -58,6 +95,15 @@ resource "aws_instance" "user" {
   }
 }
 
+resource "aws_route53_record" "user" {
+  zone_id = "Z08051092LKB6WUQCW0K4"
+  // zone id taken from domain tab itself (edit hosted zone itself)
+  name    = "user.devopsb72r.online"
+  type    = "A"
+  ttl     = 30  //300 to 30
+  records = [aws_instance.user.private_ip]
+}
+
 resource "aws_instance" "cart" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
@@ -65,6 +111,15 @@ resource "aws_instance" "cart" {
   tags = {
     Name = "cart"
   }
+}
+
+resource "aws_route53_record" "cart" {
+  zone_id = "Z08051092LKB6WUQCW0K4"
+  // zone id taken from domain tab itself (edit hosted zone itself)
+  name    = "cart.devopsb72r.online"
+  type    = "A"
+  ttl     = 30  //300 to 30
+  records = [aws_instance.cart.private_ip]
 }
 
 resource "aws_instance" "mysql" {
@@ -85,6 +140,15 @@ resource "aws_instance" "shipping" {
   }
 }
 
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z08051092LKB6WUQCW0K4"
+  // zone id taken from domain tab itself (edit hosted zone itself)
+  name    = "shipping.devopsb72r.online"
+  type    = "A"
+  ttl     = 30  //300 to 30
+  records = [aws_instance.shipping.private_ip]
+}
+
 resource "aws_instance" "rabbitmq" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
@@ -94,6 +158,15 @@ resource "aws_instance" "rabbitmq" {
   }
 }
 
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z08051092LKB6WUQCW0K4"
+  // zone id taken from domain tab itself (edit hosted zone itself)
+  name    = "rabbitmq.devopsb72r.online"
+  type    = "A"
+  ttl     = 30  //300 to 30
+  records = [aws_instance.rabbitmq.private_ip]
+}
+
 resource "aws_instance" "payment" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
@@ -101,4 +174,13 @@ resource "aws_instance" "payment" {
   tags = {
     Name = "payment"
   }
+}
+
+resource "aws_route53_record" "payment" {
+  zone_id = "Z08051092LKB6WUQCW0K4"
+  // zone id taken from domain tab itself (edit hosted zone itself)
+  name    = "payment.devopsb72r.online"
+  type    = "A"
+  ttl     = 30  //300 to 30
+  records = [aws_instance.payment.private_ip]
 }

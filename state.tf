999014e09720ba54b0542acfd2181353ba5836e0
Ramanarayana <gunjirama1999@gmail.com>
2023-07-01 22:34:14 +0530

    Roboshop_Terraform_Jenkins_dev/prod_V2

--- state.tf
+++ state.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-  bucket ="terraform-ram72"
-  key ="roboshop/prod/terraform.tfstate"
-  region="us-east-1"
+    bucket ="terraform-ram72"
+    key ="roboshop/dev/terraform.tfstate"
+    region="us-east-1"
   }
 }
 

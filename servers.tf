ad0f3c3fb721659daf324109cddf0ae7f7f23100
Ramanarayana <gunjirama1999@gmail.com>
2023-07-02 23:25:55 +0530

    Roboshop_Terraform_Jenkins_dev/prod_multi_envi_V1

--- servers.tf
+++ servers.tf
@@ -1,62 +1,12 @@
 
-data "aws_ami" "centos" {
-  owners = ["973714476881"]
-  most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice"
-  }
+
 
   //output "ami" {
   //value=data.aws_ami.centos.image_id
  // }
- data "aws_security_group"  "allow-all" {
-      name ="allow-all"
- }
 
-variable "components"{
-    default ={
-      frontend ={
-       name="frontend"
-       instance_type="t3.small"
-      }
-      mongodb ={
-       name="mongodb"
-        instance_type="t3.small"
-      }
-       catalogue ={
-         name="catalogue"
-          instance_type="t3.small"
-       }
-       redis ={
-          name="redis"
-          instance_type="t3.small"
-       }
-       user ={
-        name="user"
-         instance_type="t3.micro"
-        }
-        cart ={
-          name="cart"
-          instance_type="t3.micro"
-        }
-        mysql ={
-          name="mysql"
-          instance_type="t3.small"
-        }
-        shipping ={
-           name="shipping"
-           instance_type="t3.medium"
-        }
-        rabbitmq ={
-           name="rabbitmq"
-           instance_type="t3.small"
-        }
 
-        payment ={
-          name="payment"
-          instance_type="t3.small"
-        }
-    }
-}
+//variable "components"{}
 //Creation of instances by calling for_each
 
  resource "aws_instance" "instance" {

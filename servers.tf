528a71055e33a79155cdea745ad67b1eea7e9a45
Ramanarayana <gunjirama1999@gmail.com>
2023-07-25 10:16:57 +0530

    Roboshop-ansible_project setup_ all servers_Jenkins_V4

--- servers.tf
+++ servers.tf
@@ -11,15 +11,15 @@ module "database_servers"{
  provisioner = true
 }
 
-module "app_servers"{
- depends_on =[module.database_servers]
- for_each = var.app_servers
+module "app_servers" {
+  depends_on =[module.database_servers]
+  for_each = var.app_servers
 
- source = "./module"
- component_name = each.value["name"]
- env = var.env
- instance_type = each.value["instance_type"]
- password = lookup(each.value, "password", "null")
+  source = "./module"
+  component_name = each.value["name"]
+  env = var.env
+  instance_type = each.value["instance_type"]
+  password = lookup(each.value, "password", "null")
 }
 
 
